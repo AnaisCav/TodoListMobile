9f0bbc0fc7406bbdc58bf4a0cce9b2ef0814fccf
AnaisCav <anais.cavaille93@gmail.com>
2024-08-27 09:37:38 +0200

    ajout fonction modif et suppression + import pp

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import MainNavigator from "./navigators/MainNavigator";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import LoaderProvider from "./components/LoaderProvider";
 import "./lib/storage";
+import SettingsProvider from "./components/SettingsProvider";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <MainNavigator />
-      <StatusBar style="auto" />
+      <SettingsProvider>
+        <LoaderProvider>
+          <MainNavigator />
+          <StatusBar style="auto" />
+        </LoaderProvider>
+      </SettingsProvider>
     </SafeAreaProvider>
   );
 }
--- screens/TodosListScreen.js
+++ screens/TodosListScreen.js
@@ -4,14 +4,26 @@ import { FAB, Icon, Text } from "@rneui/themed";
 import storage from "../lib/storage";
 import { useEffect, useState } from "react";
 import TodosList from "../components/TodosList";
+import { useLoader } from "../components/LoaderProvider";
 
 function TodosListScreen({ navigation }) {
   const [list, setList] = useState([]);
+  const { setLoading } = useLoader();
 
   const getTodosList = async () => {
+    setLoading(true);
     const todosList = await storage.load({ key: "todoslist" });
     setList(listSorted(todosList));
+    setLoading(false);
   };
+
+  const deleteTodo = async (index) => {
+    const todosList = [...list];
+    todosList.splice(index, 1);
+    setList(listSorted(todosList));
+    await storage.save({ key: "todoslist", data: listSorted(todosList) });
+  };
+
   const listSorted = (initialList) =>
     initialList.sort((a, b) => {
       if (a.status === "undone" && b.status !== "undone") {
@@ -22,6 +34,7 @@ function TodosListScreen({ navigation }) {
         return 0;
       }
     });
+
   const changeCheck = async (index) => {
     const todosList = [...list];
     const todo = todosList[index];
@@ -29,6 +42,7 @@ function TodosListScreen({ navigation }) {
     await storage.save({ key: "todoslist", data: listSorted(todosList) });
     setList(todosList);
   };
+
   useEffect(() => {
     const unsubscribe = navigation.addListener("focus", () => {
       getTodosList();
@@ -48,7 +62,11 @@ function TodosListScreen({ navigation }) {
         />
         <Text h1>Ma liste!</Text>
       </View>
-      <TodosList list={list} changeCheck={changeCheck} />
+      <TodosList
+        list={list}
+        changeCheck={changeCheck}
+        deleteTodo={deleteTodo}
+      />
       <FAB
         icon={{ name: "add", color: "white" }}
         color="blue"
